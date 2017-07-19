451ce5354f720969ca46e51227c56f5b405f8171
Tri Ngo <ngominhtrint@gmail.com>
2017-07-19 19:39:32 +0700

    Milestone 3: Upload views to display images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ActiveRecord::Base
   before_save :set_default_image_path
   validates :name, presence: true
+  mount_uploader :image, ImageUploader
+
+  def image_path_or_image
+    image_path.presence || image
+  end
 
   private
 
